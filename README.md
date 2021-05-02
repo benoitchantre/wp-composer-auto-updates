be020f9c7f7e3ee8ee93795bfe86d5fd52f30f3f
Benoît Chantre <mail@benoitchantre.com>
2021-05-02 21:15:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# WP Composer Auto Updates
+
+WordPress MU-Plugin to enable maintenance and security updates when the site uses version control and `DISALLOW_FILE_MODS`.
+
+When `DISALLOW_FILE_MODS` is not set or false, WordPress will behave as if there was no VCS: plugins and themes can be installed or updated from the dashboard.
+In this scenario, `composer.lock` will get out of sync. It can be used to hand-off a project to a client.
+
+## Installation
+
+This package can be installed in the `mu-plugins` directory with `composer/installers`.
+As WordPress only load php files inside `mu-plugins` directory, it needs to be included required by a file or autoloaded using `roots/bedrock-autoloader` or similar solutions.
+
+Example of a `composer.json` to manage a WordPress site with Composer:
+
+```json
+{
+  "require": {
+    "php": ">=7.0",
+    "benoitchantre/wp-auto-updates-for-vcs": "^1.0",
+    "composer/installers": "^1.0", 
+    "johnpbloch/wordpress": "^5.5", 
+    "roots/bedrock-autoloader": "^1.0"
+  },
+  "extra": {
+    "wordpress-install-dir": "public/wp",
+    "installer-paths": {
+      "public/wp-content/mu-plugins/{$name}": [
+        "type:wordpress-muplugin"
+      ],
+      "public/wp-content/plugins/{$name}/": [
+        "type:wordpress-plugin"
+      ],
+      "public/wp-content/themes/{$name}/": [
+        "type:wordpress-theme"
+      ]
+    }
+  }
+}
+```
---
+++ wp-composer-auto-updates.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name:         WP Composer Auto Updates
+ * Description:         Enables maintenance and security updates for WordPress when VCS and DISALLOW_FILE_MODS are used.
+ * Version:             0.1.0
+ * Requires at least:   5.5
+ * Author:              Benoît Chantre
+ * Author URI:          https://benoitchantre.com
+ * License:             GPL-3.0+
+ * License URI:         http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace BenoitChantre\WP_Composer_Auto_Updates;
+
+use function wp_get_environment_type;
+use function add_filter;
+
+/**
+ * Keep automatic updates disabled for local and development environments.
+ */
+if ( in_array( wp_get_environment_type(), array( 'local', 'development' ), true ) ) {
+	return;
+}
+
+/**
+ * Allows file changes for automatic updater when DISALLOW_FILE_MODS is enabled.
+ *
+ * @param bool   $file_mod_allowed Whether file modifications are allowed.
+ * @param string $context          The usage context.
+ * @return bool
+ */
+function allow_file_mod_for_automatic_updater( bool $file_mod_allowed, string $context ) : bool {
+	if ( 'automatic_updater' === $context ) {
+		return true;
+	}
+
+	return $file_mod_allowed;
+}
+
+/**
+ * Pretends there's no VCS to allow automatic updates.
+ */
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_false' );
+
+/**
+ * Allows file changes by the automatic updater when DISALLOW_FILE_MODS is enabled.
+ * Disables auto updates except for minor core updates.
+ */
+if ( defined( 'DISALLOW_FILE_MODS' ) && DISALLOW_FILE_MODS ) {
+	add_filter( 'file_mod_allowed', __NAMESPACE__ . '\allow_file_mod_for_automatic_updater', 10, 2 );
+	add_filter( 'allow_minor_auto_core_updates', '__return_true' );
+	add_filter( 'allow_major_auto_core_updates', '__return_false' );
+	add_filter( 'themes_auto_update_enabled', '__return_false' );
+	add_filter( 'plugins_auto_update_enabled', '__return_false' );
+}
